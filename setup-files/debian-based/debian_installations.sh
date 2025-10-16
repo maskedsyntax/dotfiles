e8dafdd4c71619020b00a57483972f8688c12bf9
maskedsyntax <aftaab@aftaab.xyz>
2025-10-17 02:39:32 +0530

    update helix config and ubuntu setup file.

--- setup-files/debian-based/debian_installations.sh
+++ setup-files/debian-based/debian_installations.sh
@@ -1,119 +1,128 @@
 #!/bin/bash
 
-# Installing essential apps
-echo -e "===============================================\n"
-echo -e "Installing the following apps: \n1. Vim\n2. Neovim\n3. xsel(Clipboard for vim & neovim)\n4. nodejs\n5. npm\n6. yarn\n7. zsh\n8. tree\n9. tmux\n"
-echo -e "===============================================\n"
-sudo apt install vim-gtk3 htop neovim neofetch xsel nodejs gnome-tweaks curl wget npm yarn tree tmux ubuntu-restricted-extras
+set -e # Exit on any error
 
-# Installing extra fonts
-sudo apt install fonts-firacode fonts-cascadia-code fonts-jetbrains-mono
+echo -e "===============================================\n"
+echo -e "Updating system and installing essential apps\n"
+echo -e "===============================================\n"
+sudo apt update && sudo apt upgrade -y
 
-# Uncomment if you want to install zsh
-# sudo apt install zsh
+# Basic tools & dev essentials
+sudo apt install -y \
+    build-essential curl wget unzip git python3-pip clang clangd clang-format black shfmt \
+    vim-gtk3 htop neofetch xsel nodejs npm yarn tree tmux ubuntu-restricted-extras \
+    fonts-firacode fonts-cascadia-code fonts-jetbrains-mono fonts-ibm-plex \
+    alacritty kitty \
+    openjdk-21-jdk-headless openjdk-21-jre-headless \
+    gimp krita inkscape obs-studio vlc mpv geany dconf-editor \
+    epiphany-browser \
+    cmake ninja-build gdb lldb valgrind cloc flatpak \
+    dmz-cursor-theme thunar pavucontrol gnuplot flameshot fastfetch
 
-# Installing Alacritty Terminal (Using PPA)
+# Install Papirus Icon Pack
 echo -e "===============================================\n"
-echo -e "Installing Alacritty and Kitty Terminal \n"
+echo -e "Installing Papirus Icon Pack\n"
 echo -e "===============================================\n"
-sudo apt install alacritty kitty
+sudo sh -c "echo 'deb http://ppa.launchpad.net/papirus/papirus/ubuntu jammy main' > /etc/apt/sources.list.d/papirus-ppa.list"
+sudo wget -qO /etc/apt/trusted.gpg.d/papirus-ppa.asc 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9461999446FAF0DF770BFC9AE58A9D36647CAE7F'
+sudo apt-get update
+sudo apt-get install -y papirus-icon-theme
 
-#installing oh-my-zsh
-echo -e "===============================================\n"
-echo -e "Installing ohmyzsh\n"
-echo -e "===============================================\n"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/maskedsyntax/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
-#installing zed
-echo -e "===============================================\n"
-echo -e "Installing zed\n"
-echo -e "===============================================\n"
-curl -f https://zed.dev/install.sh | sh
-
-# installing brave browser
 echo -e "===============================================\n"
 echo -e "Installing Brave Browser\n"
 echo -e "===============================================\n"
 curl -fsS https://dl.brave.com/install.sh | sh
 
-# Language stuff
-# Installing jdk-17 & jre-17
 echo -e "===============================================\n"
-echo -e "Installing JAVA (jdk-17 & jre-17)\n"
+echo -e "Installing Helix Editor\n"
 echo -e "===============================================\n"
-sudo apt install openjdk-17-jre-headless openjdk-17-jdk-headless
+curl -LO https://github.com/helix-editor/helix/releases/latest/download/helix-x86_64-linux.tar.xz
+tar -xf helix-x86_64-linux.tar.xz
+sudo mv helix-*/hx /usr/local/bin/
+rm -rf helix-*
 
-# Install clangd
-sudo apt install clangd
+echo -e "===============================================\n"
+echo -e "Installing Oh My Zsh\n"
+echo -e "===============================================\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/maskedsyntax/ohmyzsh/master/tools/install.sh)"
 
-# Install latest nodejs lts
+echo -e "===============================================\n"
+echo -e "Installing Zed Editor\n"
+echo -e "===============================================\n"
+curl -f https://zed.dev/install.sh | sh
+
+echo -e "===============================================\n"
+echo -e "Installing Node.js LTS via NVM\n"
+echo -e "===============================================\n"
 curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 node --version
 
-# Install golang
-wget https://go.dev/dl/go1.25.1.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.25.1.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc && source ~/.zshrc
+echo -e "===============================================\n"
+echo -e "Installing Go 1.25.3\n"
+echo -e "===============================================\n"
+wget https://go.dev/dl/go1.25.3.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.25.3.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.zshrc
+source ~/.zshrc
 go version
 
-# Install Go-lsp
+echo -e "===============================================\n"
+echo -e "Installing Go tools\n"
+echo -e "===============================================\n"
 go install golang.org/x/tools/gopls@latest
+go install github.com/nametake/golangci-lint-langserver@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 
-# installing Epiphany browser
 echo -e "===============================================\n"
-echo -e "Installing Epiphany Browser\n"
+echo -e "Installing Rust and tools\n"
 echo -e "===============================================\n"
-sudo apt install epiphany-browser
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup component add rustfmt rust-analyzer
+cargo install taplo-cli
 
-
-# installing other tools
 echo -e "===============================================\n"
-echo -e "Installing other tools\n"
+echo -e "Installing Language Servers & Formatters\n"
 echo -e "===============================================\n"
-sudo apt install gimp krita obs-studio vlc geany dconf-editor
+npm install -g \
+    pyright \
+    bash-language-server \
+    yaml-language-server \
+    prettier \
+    vscode-langservers-extracted
 
-
-# installing Sublime text editor
 echo -e "===============================================\n"
-echo -e "Installing sublime-text-4\n"
+echo -e "Installing Sublime Text 4\n"
 echo -e "===============================================\n"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo -e "(Installing the stable branch)\n"
+sudo apt-get install apt-transport-https -y
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
-
-
-# installing Virtual Box
-# echo -e "===============================================\n"
-# echo -e "Installing Virtual Box\n"
-# echo -e "===============================================\n"
-# sudo apt install virtualbox virtualbox-ext-pack
-
-
-# Installing vim-plug for vim and neovim
-echo -e "===============================================\n"
-echo -e "Installing vim-plug for vim\n"
-echo -e "===============================================\n"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo apt-get install sublime-text -y
 
 echo -e "===============================================\n"
-echo -e "Installing vim-plug for neovim\n"
+echo -e "Installing lldb-dap-20 and creating symlink\n"
 echo -e "===============================================\n"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
+sudo apt install -y lldb-dap-20
+sudo ln -sf /usr/bin/lldb-dap-20 /usr/local/bin/lldb-dap
 
 echo -e "===============================================\n"
-echo -e "                      DONE                       "
+echo -e "Installing Chromium via Flatpak\n"
 echo -e "===============================================\n"
+flatpak install -y flathub org.chromium.Chromium
 
+echo -e "===============================================\n"
+echo -e "Configuring Git\n"
+echo -e "===============================================\n"
+git config --global user.name "maskedsyntax"
+git config --global user.email "aftaab@aftaab.xyz"
+git config --global credential.helper store
+git config --global core.editor "hx"
 
-
-
-
+echo -e "===============================================\n"
+echo -e "                    DONE                       "
+echo -e "===============================================\n"
