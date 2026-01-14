9b38c2d4d535987c7b942b4f61d9509eca1ac825
maskedsyntax <aftaab@aftaab.xyz>
2026-01-14 22:47:53 +0530

    Update polybar config

--- polybar/mic.sh
+++ polybar/mic.sh
@@ -1,10 +1,16 @@
 
 #!/usr/bin/env bash
 
+# if pactl get-source-mute @DEFAULT_SOURCE@ | grep -q "yes"; then
+#   echo "%{B#CC000000}%{F#ff5555}[Mic: muted]%{B-}%{F-}"
+# else
+#   vol=$(pactl get-source-volume @DEFAULT_SOURCE@ | awk '{print $5}' | head -n1)
+#   echo "%{B#CC000000}%{F#E57373}Mic: $vol%{B-}%{F-}"
+# fi
+
 if pactl get-source-mute @DEFAULT_SOURCE@ | grep -q "yes"; then
-  echo "%{B#b3ff5555}%{F#000000}[Mic: muted]%{B-}%{F-}"
+  echo "muted"
 else
-  vol=$(pactl get-source-volume @DEFAULT_SOURCE@ | awk '{print $5}' | head -n1)
-  echo "%{B#b3ffffff}%{F#000000}[Mic: $vol]%{B-}%{F-}"
+  pactl get-source-volume @DEFAULT_SOURCE@ | awk '{print $5}' | head -n1
 fi
 
